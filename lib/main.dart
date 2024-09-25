626d443ec62b8da5cb2c19e5e444bfbaab05c533
kent0505 <otabekyusupov0550@gmail.com>
2024-09-26 00:36:36 +0500

    removed bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'core/config/router.dart';
 import 'core/config/themes.dart';
 import 'core/utils.dart';
-import 'features/home/bloc/home_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,16 +20,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     precacheImages(context);
-
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context) => HomeBloc()),
-      ],
-      child: MaterialApp.router(
-        debugShowCheckedModeBanner: false,
-        theme: theme,
-        routerConfig: routerConfig,
-      ),
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      theme: theme,
+      routerConfig: routerConfig,
     );
   }
 }
